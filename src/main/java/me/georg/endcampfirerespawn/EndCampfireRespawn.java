71240e04495a0d1d93f5b32fe8968b5998b704d6
ItzF555g <peeterposti@gmail.com>
2026-07-28 09:23:12 +0000

    Update EndCampfireRespawn.java

--- src/main/java/me/georg/endcampfirerespawn/EndCampfireRespawn.java
+++ src/main/java/me/georg/endcampfirerespawn/EndCampfireRespawn.java
@@ -2,14 +2,21 @@ package me.georg.endcampfirerespawn;
 
 import me.georg.endcampfirerespawn.listeners.CampfireListener;
 import me.georg.endcampfirerespawn.listeners.ExplosionListener;
+import me.georg.endcampfirerespawn.managers.SpawnManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EndCampfireRespawn extends JavaPlugin {
 
     private static EndCampfireRespawn instance;
 
+    private SpawnManager spawnManager;
+
     public static EndCampfireRespawn getInstance() {
         return instance;
+    }
+
+    public SpawnManager getSpawnManager() {
+        return spawnManager;
     }
 
     @Override
@@ -19,20 +26,22 @@ public class EndCampfireRespawn extends JavaPlugin {
 
         saveDefaultConfig();
 
+        spawnManager = new SpawnManager(this);
+
         getServer().getPluginManager().registerEvents(
                 new CampfireListener(), this);
 
         getServer().getPluginManager().registerEvents(
                 new ExplosionListener(), this);
 
-        getLogger().info("EndCampfireRespawn enabled.");
+        getLogger().info("EndCampfireRespawn Enabled");
 
     }
 
     @Override
     public void onDisable() {
 
-        getLogger().info("EndCampfireRespawn disabled.");
+        getLogger().info("Plugin Disabled");
 
     }
 
